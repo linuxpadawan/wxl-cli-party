3acad4a4ae03386c45c5cb0d86ac5b69dceac5ec
live and direct from cli-party <info@linuxpadawan.net>
2016-01-21 09:18:39 +0000

    added script to make new vm

---
+++ setup-vm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# run this to set up a new cli-party
+# always git pull!
+
+# add to list on a blank line between the parens
+# syntax: "directory or file"\<Space>
+# directories are handled recusively
+entries=(\ 
+".c9"\ 
+".gitconfig"\ 
+".gitignore"\ 
+".linux_logo"\ 
+".tmux.conf"\ 
+".vimrc"\ 
+"README.md"\ 
+)
+
+cd $HOME/workspace
+
+for i in "${entries[@]}"
+do
+	rm $HOME/"${i}"
+	ln -s $HOME/workspace/"${i}" $HOME/"${i}"
+done
