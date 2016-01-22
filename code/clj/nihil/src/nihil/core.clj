d6f9159c29fdeeb3b0684135cf4837c3e98c2d2f
live and direct: cli-party <info@linuxpadawan.net>
2016-01-22 01:36:19 +0000

    finally worked out all the code!

--- code/clj/nihil/src/nihil/core.clj
+++ code/clj/nihil/src/nihil/core.clj
@@ -4,11 +4,10 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def x (set '(nil)))
-  (defn test-null
-    [x]
-    (empty x)
-    (not-empty x)
-    (true? x)
-    (false? x))
-  (test-null x))
+  (let [f #(
+    (println "empty?" (empty? %)) 
+    (println "empty" (not (nil? (empty %)))) 
+    (println "not-empty" (not (nil? (not-empty %)))) 
+    (println "true?" (true? %)) 
+    (println "false?" (false? %)))] 
+  (f (set '(nil)))))
